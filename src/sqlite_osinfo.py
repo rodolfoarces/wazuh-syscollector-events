d8694025288e81e2c2334cb148830cff95810d56
Diego Guerrero <diego.guerrero@gmail.com>
2025-01-13 12:39:28 +0000

    Extraction of data from one client

--- src/sqlite_osinfo.py
+++ src/sqlite_osinfo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-##/var/ossec/framework/python/bin/python3 /home/wazuh-user/syscollector-report/src/sqlite.py
+##/var/ossec/framework/python/bin/python3 /home/wazuh-user/syscollector-report/src/sqlite_osinfo.py
 
 import os
 import sqlite3
@@ -11,10 +11,10 @@ def show_osinfo(db_path):
     try:
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM dbsync_osinfo")
+        cursor.execute("SELECT * FROM sys_osinfo")
         osinfo = cursor.fetchall()
         if osinfo:
-            print("Tables in the osinfo database:")
+            print("Data in the osinfo database:")
             for row in osinfo:
                 print(row)
         else:
@@ -23,12 +23,47 @@ def show_osinfo(db_path):
     except sqlite3.Error as e:
         print(f"An error occurred: {e}")
 
+def show_hotfixes(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sys_hotfixes")
+        hotfixes = cursor.fetchall()
+        if hotfixes:
+            print("Data in the hotfixes database:")
+            for row in hotfixes:
+                print(row)
+        else:
+            print("No hotfixes data found in the database.")
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+def show_hwinfo(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sys_hwinfo")
+        hwinfo = cursor.fetchall()
+        if hwinfo:
+            print("Data in the hwinfo database:")
+            for row in hwinfo:
+                print(row)
+        else:
+            print("No hwinfo data found in the database.")
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
 def main():
     #db_path = os.path.expanduser("~/410.db")
-    db_path = os.path.expanduser("/var/ossec/queue/syscollector/db/local.db")
+    #db_path = os.path.expanduser("/var/ossec/queue/syscollector/db/local.db")
+    db_path = os.path.expanduser("/var/ossec/queue/db/002.db")
     if check_db_access(db_path):
         print("Database file is accessible.")
         show_osinfo(db_path)
+        show_hotfixes(db_path)
+        show_hwinfo(db_path)
     else:
         print("Database file is not accessible.")
 
