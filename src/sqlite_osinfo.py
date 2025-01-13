b2510a2fe15fab968ec4809e07bcf7078eca23dd
Diego Guerrero <diego.guerrero@gmail.com>
2025-01-13 12:59:59 +0000

    Extraction of all info from one endpoint

--- src/sqlite_osinfo.py
+++ src/sqlite_osinfo.py
@@ -55,6 +55,102 @@ def show_hwinfo(db_path):
     except sqlite3.Error as e:
         print(f"An error occurred: {e}")
 
+def show_netaddr(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sys_netaddr")
+        netaddr = cursor.fetchall()
+        if netaddr:
+            print("Data in the netaddr database:")
+            for row in netaddr:
+                print(row)
+        else:
+            print("No netaddr data found in the database.")
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+def show_netiface(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sys_netiface")
+        netiface = cursor.fetchall()
+        if netiface:
+            print("Data in the netiface database:")
+            for row in netiface:
+                print(row)
+        else:
+            print("No netiface data found in the database.")
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+def show_netproto(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sys_netproto")
+        netproto = cursor.fetchall()
+        if netproto:
+            print("Data in the netproto database:")
+            for row in netproto:
+                print(row)
+        else:
+            print("No netproto data found in the database.")
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+def show_ports(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sys_ports")
+        ports = cursor.fetchall()
+        if ports:
+            print("Data in the ports database:")
+            for row in ports:
+                print(row)
+        else:
+            print("No ports data found in the database.")
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+def show_processes(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sys_processes")
+        processes = cursor.fetchall()
+        if processes:
+            print("Data in the processes database:")
+            for row in processes:
+                print(row)
+        else:
+            print("No processes data found in the database.")
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+def show_programs(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sys_programs")
+        programs = cursor.fetchall()
+        if programs:
+            print("Data in the programs database:")
+            for row in programs:
+                print(row)
+        else:
+            print("No programs data found in the database.")
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
 def main():
     #db_path = os.path.expanduser("~/410.db")
     #db_path = os.path.expanduser("/var/ossec/queue/syscollector/db/local.db")
@@ -64,6 +160,12 @@ def main():
         show_osinfo(db_path)
         show_hotfixes(db_path)
         show_hwinfo(db_path)
+        show_netaddr(db_path)
+        show_netiface(db_path)
+        show_netproto(db_path)
+        show_ports(db_path)
+        show_processes(db_path)
+        show_programs(db_path)
     else:
         print("Database file is not accessible.")
 
