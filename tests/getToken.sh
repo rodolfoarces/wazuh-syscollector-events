2d51f5cc5f0bb9e64120748b79fb8c06d573e2db
Rodolfo D. Arce Sannemann <rodolfo.arce@wazuh.com>
2025-02-19 08:59:34 -0300

    Eliminando pruebas de concepto

--- tests/getToken.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-USER=wazuh
-PASSWORD=wazuh
-HOST_IP=127.0.0.1
-JWT_TOKEN="$(curl -u $USER:$PASSWORD -k -X POST "https://$HOST_IP:55000/security/user/authenticate"| jq -r '.data.token')"
-echo JWT_TOKEN=$JWT_TOKEN
-#curl -k -X GET https://$HOST_IP:55000/ -H "Authorization: Bearer $JWT_TOKEN"
